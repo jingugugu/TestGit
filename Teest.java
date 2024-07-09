f1780632d5fd555f462de01e231a5e71864d7ac5
진민지 <67037321+realmingji@users.noreply.github.com>
2024-07-09 17:43:24 +0900

    home

--- Teest.java
+++ Teest.java
@@ -2,5 +2,6 @@ public class Teest {
     public static void main(String[] args) {
         System.out.println("아무거나");
         System.out.println("아무거나11");
+        System.out.println("아무거나11");
     }
 }
